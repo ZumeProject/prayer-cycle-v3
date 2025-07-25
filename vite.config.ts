09a32b27017cc3628fe3812537436ab1cf26e95d
corsacca <corsacca@gmail.com>
2025-07-25 11:23:52 +0100

    Github pages fix mime type issue

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/prayer-cycle-v3/',
   plugins: [
     vue(),
     vueDevTools(),
